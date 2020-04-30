805f5346ba105a01bd16ae589e9f56dfb997b9fb
Robo-Mike <mike.rawlinson@yahoo.com>
2020-04-30 18:57:05 +0100

    Some n-ftdi addon stuff working

--- src/services/index.js
+++ src/services/index.js
@@ -4,25 +4,50 @@ import {Data as DeviceInfoListData}  from './deviceinfolistdata.js'
 let FTD2XX = window.require('n-ftdi')
 
 
-async function Test(){
-  console.log(await 'real list retrieved ' + FTD2XX.FTDI.getDeviceList())
+// async function Test(){
+//   console.log( 'calling async get device list ')
+//   const list = await FTD2XX.FTDI.getDeviceList()
+//   console.log( 'real list retrieved ' + list.deviceList.length)
+// }
+
+//compare contrast explicit promise against async method style
+
+export const getDeviceInfoList = async  () => {
+  const myList = await FTD2XX.FTDI.getDeviceList()
+  return myList.deviceList.map((value,index) => ({serialNo: value.serialNumber, description: value.description, productCode: 'XYZ'}) )
 }
 
-export const getDeviceInfoList = () => {
+export const getDeviceInfoListMock = () => {
   /*fake taking sometime and make non blockng with settimeout*/
-  Test()
+
   return new Promise((success, failure) => {
     setTimeout( ()=> { success( DeviceInfoListData ) } ,500)
   } )
 
 }
 
-export const openDevice = (serialNo) => {
+export const openDeviceMock = (serialNo) => {
   /*fake taking sometime and make non blockng with settimeout*/
   const openedDevice = DeviceInfoListData.filter(device => device.serialNo === serialNo )[0]
   console.log('Opening device with serial no ' + openedDevice.serialNo)
   return new Promise((success, failure) => {
     setTimeout( ()=> { success( {...openedDevice, currentPosition : 5.0, connected : true, targetPosition : 0 } ) } ,500)
   } )
+}
 
+export const openDevice = async (serialNo) => {
+  console.log('Opening device with serial no ' + serialNo)
+  const status = await FTD2XX.FTDI.openBySerialNumber(serialNo)
+  if (status == FTD2XX.FTDI.FT_STATUS.FT_OK )
+  {
+    console.log ('Status is' + status)
+    //TODO wrap write and get status + read with APT function to get position
+    const deviceInfo = await  FTD2XX.FTDI.getDeviceInfo()
+    return {serialNo: deviceInfo.serialNumber, description: deviceInfo.description, productCode: 'XYZ',currentPosition : 5.0, connected : true, targetPosition : 0 }
+  }
+  else {
+    console.log (status)
+    //TODO cant connect message
+    return {}
+  }
 }
