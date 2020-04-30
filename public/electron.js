2ebac5c7126e29d9f795b078451a8970474e7288
Robo-Mike <mike.rawlinson@yahoo.com>
2020-04-30 12:17:16 +0100

    Added n-ftdi add on tidied up dev start so browser not launched but pages are still served from webpack-dev-sever

--- public/electron.js
+++ public/electron.js
@@ -13,14 +13,20 @@ let mainWindow;
 
 function createWindow() {
     // Create the browser window.
-    mainWindow = new BrowserWindow({width: 800, height: 600});
+    mainWindow = new BrowserWindow({width: 800, height: 600,
+  webPreferences: {
+    nodeIntegration: true
+  }});
 
     // load the index.html of the app.
-    const startUrl = process.env.ELECTRON_START_URL || url.format({
+    let startUrl = process.env.ELECTRON_START_URL || url.format({
         pathname: path.join(__dirname, '/../build/index.html'),
         protocol: 'file:',
         slashes: true
     });
+
+    //TODO remove test
+    //startUrl ='http://localhost:3000'
 
 
 
--- src/services/index.js
+++ src/services/index.js
@@ -1,7 +1,16 @@
 import {Data as DeviceInfoListData}  from './deviceinfolistdata.js'
+//mix of require and imports yeuch note import doesnt work for ftdi addon module
+//import * as FTD2XX from 'n-ftdi'
+let FTD2XX = window.require('n-ftdi')
+
+
+async function Test(){
+  console.log(await 'real list retrieved ' + FTD2XX.FTDI.getDeviceList())
+}
 
 export const getDeviceInfoList = () => {
   /*fake taking sometime and make non blockng with settimeout*/
+  Test()
   return new Promise((success, failure) => {
     setTimeout( ()=> { success( DeviceInfoListData ) } ,500)
   } )
